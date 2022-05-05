05f00c2a6f2e08009a0f9412b226dc18c7dea8c3
CodedOre <47981497+CodedOre@users.noreply.github.com>
2022-05-05 19:35:35 +0200

    Fix not recognized last player positioning position

--- scripts/board/Position.gd
+++ scripts/board/Position.gd
@@ -19,7 +19,7 @@ func get_max_players () -> int:
 
 # - Return the origin for players on this position -
 func get_player_position (player_sum : int, player_id : int) -> Vector3:
-	var array_id : int = player_sum if player_sum < len (player_positions) else len (player_positions) - 1
+	var array_id : int = player_sum if player_sum < len (player_positions) else len (player_positions)
 	var pos_array : PoolVector3Array = player_positions [array_id - 1]
 	var pos_id : int = player_id if player_id < len (pos_array) else len (pos_array) - 1
 	return global_transform.origin + pos_array [pos_id]
