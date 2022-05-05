072c0b6407b84c37840611f3f3fb6b1b9292e51b
CodedOre <47981497+CodedOre@users.noreply.github.com>
2022-05-05 15:48:23 +0200

    Drag the player around

--- scripts/game/PlayerDrag.gd
+++ scripts/game/PlayerDrag.gd
@@ -6,6 +6,12 @@
 extends Node
 
 
+# -- Constants --
+
+# - How much the player hovers over the board -
+const DRAG_HOVER : Vector3 = Vector3 (0.0, 0.5, 0.0)
+
+
 # -- Signals --
 
 # - Notify RallyGame about used moves -
@@ -25,13 +31,19 @@ var __player_dragged : bool = false
 # - Begins the drag action -
 func drag_player (player : KinematicBody) -> void:
 	# Check if the player is active (and can be dragged)
-	if player != __current_player:
-		return
-	pass
+	if player == __current_player:
+		__player_dragged = true
 
 # - Move the player while dragging -
 func move_player (position : Vector3) -> void:
-	pass
+	# Check if we drag
+	if not __player_dragged:
+		return
+	# Ignore if Vector is invalid
+	if position == Vector3.INF:
+		return
+	# Set the player to mouse position with a bit of hover
+	__current_player.global_transform.origin = position + DRAG_HOVER
 
 # - Stops the drag action -
 func drop_player (player : KinematicBody) -> void:
@@ -40,3 +52,4 @@ func drop_player (player : KinematicBody) -> void:
 # - Updates the current player from RallyGame -
 func update_current_player (player : KinematicBody, moves : int) -> void:
 	__current_player = player
+	__left_moves = moves
