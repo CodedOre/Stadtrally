d17c073c59019fb5eeb0d9d74f0900fba84ee18a
CodedOre <47981497+CodedOre@users.noreply.github.com>
2022-05-05 15:13:49 +0200

    Connect signals for drag actions

--- scripts/game/PlayerDrag.gd
+++ scripts/game/PlayerDrag.gd
@@ -4,3 +4,23 @@
 # Manages drag and drop actions of an player.
 
 extends Node
+
+# -- Variables --
+
+# - The currently dragged player -
+var __dragged_player : KinematicBody = null
+
+
+# -- Functions --
+
+# - Begins the drag action -
+func drag_player (player : KinematicBody) -> void:
+	pass
+
+# - Move the player while dragging -
+func move_player (position : Vector3) -> void:
+	pass
+
+# - Stops the drag action -
+func drop_player (player : KinematicBody) -> void:
+	pass
