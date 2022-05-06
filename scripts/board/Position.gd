c3e903c61af399e5da7600124fe871ac4a7ba4c2
CodedOre <47981497+CodedOre@users.noreply.github.com>
2022-05-06 13:09:28 +0200

    Set the frame color for the Position

--- scripts/board/Position.gd
+++ scripts/board/Position.gd
@@ -3,12 +3,40 @@
 # --- Position Script ---
 # Data for a single position on the board.
 
+tool
 extends Spatial
+
+# -- Enums --
+
+# - What status the position is displaying -
+enum FeedbackStatus {
+	NONE,
+	CURRENT,
+	PARTIAL_MOVE,
+	FULL_MOVE,
+	WRONG
+}
+
 
 # -- Properties --
 
+# - The currently active status -
+export (FeedbackStatus) var feedback setget set_feedback, get_feedback
+
+# - The colors assigned to a status -
+export (Color, RGB) var color_current
+export (Color, RGB) var color_partial_move
+export (Color, RGB) var color_full_move
+export (Color, RGB) var color_wrong
+
 # - Positions for n players on the Position -
 export (Array, PoolVector3Array) var player_positions
+
+
+# -- Variables --
+
+# - Stores the currently active status -
+var __current_feedback : int = FeedbackStatus.NONE
 
 
 # -- Functions --
@@ -23,3 +51,31 @@ func get_player_position (player_sum : int, player_id : int) -> Vector3:
 	var pos_array : PoolVector3Array = player_positions [array_id - 1]
 	var pos_id : int = player_id if player_id < len (pos_array) else len (pos_array) - 1
 	return global_transform.origin + pos_array [pos_id]
+
+# - Get the feedback status -
+func get_feedback () -> int:
+	return __current_feedback
+
+# - Set a new feedback status -
+func set_feedback (value : int) -> void:
+	# Get the material for the frame
+	var frame_material : ShaderMaterial = $FrameMesh.get_active_material (0)
+	# Adapt the frame for different states
+	match value:
+		FeedbackStatus.CURRENT:
+			__current_feedback = value
+			frame_material.set_shader_param ("frame_color", color_current)
+		FeedbackStatus.PARTIAL_MOVE:
+			__current_feedback = value
+			frame_material.set_shader_param ("frame_color", color_partial_move)
+		FeedbackStatus.FULL_MOVE:
+			__current_feedback = value
+			frame_material.set_shader_param ("frame_color", color_full_move)
+		FeedbackStatus.WRONG:
+			__current_feedback = value
+			frame_material.set_shader_param ("frame_color", color_wrong)
+		_:
+			# Non-recognized values are set to FeedbackStatus.NONE
+			__current_feedback = FeedbackStatus.NONE
+	# Set the visibility of the frame
+	$FrameMesh.visible = not __current_feedback == FeedbackStatus.NONE
