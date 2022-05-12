29b242d96a1087eb87dde6b189e196e08ec01725
CodedOre <47981497+CodedOre@users.noreply.github.com>
2022-05-12 14:49:21 +0200

    Add Tween to interpolate position changes

--- scripts/game/Player.gd
+++ scripts/game/Player.gd
@@ -3,7 +3,7 @@
 # --- Player Script ---
 # Player figure and data pertaining to a player.
 
-extends Spatial
+extends KinematicBody
 
 # -- Enums --
 
@@ -30,6 +30,9 @@ const COLOR_MATERIAL : Dictionary = {
 	PlayerColors.PURPLE: preload ("res://resources/material/game/Player/Purple.tres")
 }
 
+# - The movement of the player when using the Tween -
+const MOVEMENT_TIME : float = 0.75
+
 
 # -- Nodes --
 
@@ -43,6 +46,12 @@ onready var mesh_node : MeshInstance = $Mesh
 export (PlayerColors) var color
 
 
+# -- Variables --
+
+# - A Tween to animate the movement of the player to a new position -
+var move_tween : Tween
+
+
 # -- Functions --
 
 # - Runs when scene is added to the scene tree
@@ -53,3 +62,19 @@ func _ready() -> void:
 	# Set the material for the mesh to the color
 	var color_material : Material = COLOR_MATERIAL [color]
 	mesh_node.set_surface_material (0, color_material)
+	# Create the movement tween
+	move_tween = Tween.new ()
+	add_child (move_tween)
+
+# - Set a target position the player should be moved to -
+func set_target_position (target : Vector3) -> void:
+	# Create the current and target transforms
+	var current_transform : Transform = global_transform
+	var target_transform : Transform = Transform (current_transform.basis, target)
+	# Start the tween and set the transform interpolation
+	# warning-ignore:return_value_discarded
+	move_tween.interpolate_property (self, "global_transform", current_transform, target_transform, MOVEMENT_TIME, Tween.TRANS_EXPO, Tween.EASE_IN_OUT)
+	# warning-ignore:return_value_discarded
+	move_tween.start ()
+	# Wait until the interpolation has completed
+	yield (move_tween, "tween_completed")
