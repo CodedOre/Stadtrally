fe1ff82babd17306a7c31d9bf312080bf5181aa6
CodedOre <47981497+CodedOre@users.noreply.github.com>
2022-06-08 10:22:43 +0200

    Turn the camera gimbal on input

--- scripts/game/GameCamera.gd
+++ scripts/game/GameCamera.gd
@@ -5,8 +5,27 @@
 
 extends Spatial
 
+
+# -- Constants --
+
+# - Limits how fast the camera moves -
+const CAMERA_MOVE_SPEED : float = 0.02
+
+
+# -- Nodes --
+
+# - The "gimbal" for the camera
+onready var outer_gimbal : Spatial = $OuterGimbal
+onready var inner_gimbal : Spatial = $OuterGimbal/InnerGimbal
+
+
 # -- Functions --
 
 # - Turns the camera according to the retrieved input -
 func turn_camera (turn_vector : Vector2) -> void:
-	pass
+	# Get the values into floats (and clamp them when needed)
+	var horizontal_move : float = turn_vector.x * CAMERA_MOVE_SPEED
+	var vertical_move : float = turn_vector.y * CAMERA_MOVE_SPEED
+	# Move each gimbal part into one direction
+	outer_gimbal.rotate_object_local (Vector3.UP, horizontal_move)
+	inner_gimbal.rotate_object_local (Vector3.RIGHT, vertical_move)
